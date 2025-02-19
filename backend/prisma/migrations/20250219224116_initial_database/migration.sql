10957d3156b575890ce2d5564db0fae166236c1d
Nao Gobaru <take57bamboo@gmail.com>
2025-02-19 15:46:25 -0700

    fix database schema

--- backend/prisma/migrations/20250219224116_initial_database/migration.sql
+++ backend/prisma/migrations/20250219224116_initial_database/migration.sql
@@ -3,8 +3,19 @@ CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "profile_image_url" TEXT NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_families" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "profile_image_url" TEXT NOT NULL,
+
+    CONSTRAINT "user_families_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -12,7 +23,7 @@ CREATE TABLE "events" (
     "id" SERIAL NOT NULL,
     "user_id" INTEGER NOT NULL,
     "title" TEXT NOT NULL,
-    "thumbnail" TEXT NOT NULL,
+    "thumbnail_url" TEXT NOT NULL,
     "start_time" TIMESTAMP(3) NOT NULL,
     "end_time" TIMESTAMP(3) NOT NULL,
     "country" TEXT NOT NULL,
@@ -23,33 +34,35 @@ CREATE TABLE "events" (
     "address2" TEXT NOT NULL,
     "is_ask_restrictions" BOOLEAN NOT NULL,
     "event_type" TEXT NOT NULL,
+    "design_info" TEXT NOT NULL,
     "theme" TEXT NOT NULL,
     "note_for_things_to_buy" TEXT NOT NULL,
-    "note_for_things_to_bring" TEXT NOT NULL,
+    "note_for_necessities" TEXT NOT NULL,
 
     CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "events_particpants" (
+CREATE TABLE "event_participants" (
     "event_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
+    "user_family_id" INTEGER NOT NULL,
     "is_accepted" BOOLEAN NOT NULL,
     "is_attended" BOOLEAN NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "participants_restrictions" (
+CREATE TABLE "participant_restrictions" (
     "id" SERIAL NOT NULL,
     "event_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
     "note" TEXT NOT NULL,
 
-    CONSTRAINT "participants_restrictions_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "participant_restrictions_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "things_to_bring" (
+CREATE TABLE "necessities" (
     "event_id" INTEGER NOT NULL,
     "user_id" INTEGER NOT NULL,
     "order" INTEGER NOT NULL,
@@ -80,44 +93,50 @@ CREATE TABLE "timelines" (
 );
 
 -- CreateTable
-CREATE TABLE "media" (
+CREATE TABLE "medias" (
     "id" SERIAL NOT NULL,
     "event_id" INTEGER NOT NULL,
     "image_title" TEXT NOT NULL,
     "image_url" TEXT NOT NULL,
 
-    CONSTRAINT "media_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "medias_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "events_particpants_event_id_user_id_key" ON "events_particpants"("event_id", "user_id");
+CREATE UNIQUE INDEX "event_participants_event_id_user_id_user_family_id_key" ON "event_participants"("event_id", "user_id", "user_family_id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "things_to_bring_event_id_user_id_order_key" ON "things_to_bring"("event_id", "user_id", "order");
+CREATE UNIQUE INDEX "necessities_event_id_user_id_order_key" ON "necessities"("event_id", "user_id", "order");
+
+-- AddForeignKey
+ALTER TABLE "user_families" ADD CONSTRAINT "user_families_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "events" ADD CONSTRAINT "events_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "events_particpants" ADD CONSTRAINT "events_particpants_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_participants" ADD CONSTRAINT "event_participants_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "events_particpants" ADD CONSTRAINT "events_particpants_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_participants" ADD CONSTRAINT "event_participants_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "participants_restrictions" ADD CONSTRAINT "participants_restrictions_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event_participants" ADD CONSTRAINT "event_participants_user_family_id_fkey" FOREIGN KEY ("user_family_id") REFERENCES "user_families"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "participants_restrictions" ADD CONSTRAINT "participants_restrictions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "participant_restrictions" ADD CONSTRAINT "participant_restrictions_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "things_to_bring" ADD CONSTRAINT "things_to_bring_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "participant_restrictions" ADD CONSTRAINT "participant_restrictions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "things_to_bring" ADD CONSTRAINT "things_to_bring_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "necessities" ADD CONSTRAINT "necessities_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "necessities" ADD CONSTRAINT "necessities_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "things_to_buy" ADD CONSTRAINT "things_to_buy_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -126,4 +145,4 @@ ALTER TABLE "things_to_buy" ADD CONSTRAINT "things_to_buy_event_id_fkey" FOREIGN
 ALTER TABLE "timelines" ADD CONSTRAINT "timelines_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "media" ADD CONSTRAINT "media_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "medias" ADD CONSTRAINT "medias_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
