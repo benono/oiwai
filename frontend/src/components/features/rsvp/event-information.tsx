01741ce1d1e882ddd0d87589147899d0456f4ae2
Risa Yamamoto <yamari2422@gmail.com>
2025-02-19 13:29:07 -0800

    feat: add the page for event information

--- frontend/src/components/features/rsvp/event-information.tsx
+++ frontend/src/components/features/rsvp/event-information.tsx
@@ -1,6 +1,24 @@
+import Image from "next/image";
+
 const EventInformation = () => {
   return (
-    <div>event-information</div>
-  )
-}
-export default EventInformation
+    <section className="w-full space-y-4">
+      <Image
+        src="/sample-thumbnail.png"
+        width={200}
+        height={200}
+        alt="thumbnail"
+        className="w-full"
+      />
+      <div className="space-y-2 px-4">
+        <h1 className="text-2xl font-bold">Emily’s Birthday Party🚀</h1>
+        <div>
+          <p className="font-semibold">Wednesday, February 10</p>
+          <p className="text-sm font-medium">0:00 PM - 3:00 PM</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EventInformation;
