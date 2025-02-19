45c7cc65100d1cc343eccd6730cdeb2750ea227b
kakimaru <memimei0306@gmail.com>
2025-02-20 07:23:07 +0900

    feat/add base html and images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,219 @@
+import { Button } from "@/components/ui/button";
+import { Lato } from "next/font/google";
 import Image from "next/image";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import hostGroupPic1 from "@/public/images/eventhost_group_image1.png";
+import hostGroupPic2 from "@/public/images/eventhost_group_image2.png";
+import hostGroupPic3 from "@/public/images/eventhost_group_image3.png";
+import hostMainPic from "@/public/images/eventhost_main_image.png";
+import hostSubPic1 from "@/public/images/eventhost_sub_image1.png";
+import hostSubPic2 from "@/public/images/eventhost_sub_image2.png";
+import linkedInLogo from "@/public/images/LI-In-Bug.png";
+import mainPic from "@/public/images/main_image.png";
+import guestMainPic from "@/public/images/participants_main_image.png";
+import guestSubPic1 from "@/public/images/participants_sub_image1.png";
+import guestSubPic2 from "@/public/images/participants_sub_image2.png";
+import Link from "next/link";
+
+const lato = Lato({
+  weight: ["100", "300", "400", "700", "900"],
+  variable: "--font-lato",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <header className="bg-white px-4 py-3">
+        <div className="flex items-start justify-between gap-2.5">
+          <h1 className="text-text text-xl font-bold">Oiwai</h1>
+          <Button className="h-auto rounded-full px-4 py-1.5 text-xs font-bold">
+            CREATE EVENT
+          </Button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </header>
+      <section className="bg-white px-4">
+        <section className="grid justify-items-center gap-6 text-center">
+          <h1 className={`text-text text-[32px] font-bold ${lato.className}`}>
+            Celebrate & Connect <br />
+            with Ease!
+          </h1>
+          <p className="text-text text-sm">
+            Effortlessly plan and manage your child&apos;s birthday or special
+            event. Create personalized invitations and share them with friends,
+            all in one place!
+          </p>
+          <Button className="h-auto w-fit rounded-full px-10 py-3 text-base font-bold">
+            CREATE EVENT
+          </Button>
+          <div className="relative h-[300px] w-full">
+            <Image src={mainPic} alt="Oiwai image" />
+          </div>
+        </section>
+
+        <section>
+          <Tabs defaultValue="host">
+            <TabsList className="w-full">
+              <TabsTrigger value="host" className="w-full">
+                EVENT HOST
+              </TabsTrigger>
+              <TabsTrigger value="participants" className="w-full">
+                PARTICIPANTS
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="host">
+              <section>
+                <div>
+                  <Image src={hostMainPic} alt="For the event host" />
+                  <h2
+                    className={`text-accentGreen text-2xl font-bold ${lato.className}`}
+                  >
+                    Create invitations effortlessly
+                  </h2>
+                  <p className="text-text text-base">
+                    Easily create event invitations with a few tap.
+                  </p>
+                </div>
+                <div>
+                  <div>
+                    <h3>Get AI-generated venue suggestions</h3>
+                    <p>
+                      Find recommended spots on the map based on activities.
+                    </p>
+                    <Image
+                      src={hostSubPic1}
+                      alt="AI gives recommendations for good places."
+                      className="w-full"
+                    />
+                  </div>
+                  <div>
+                    <h3>Various invitation theme</h3>
+                    <p>
+                      Find recommended spots on the map based on activities.
+                    </p>
+                    <Image
+                      src={hostSubPic2}
+                      alt="Various invitation theme"
+                      className="w-full"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <h2>
+                    Make your event even more special with an exclusive group
+                    for participants.
+                  </h2>
+                  <div>
+                    <div>
+                      <Image
+                        src={hostGroupPic1}
+                        alt="AI suggests your schedule"
+                      />
+                      <h3>AI suggests your schedule</h3>
+                      <p>
+                        Just enter a few details, and your timeline is ready!
+                      </p>
+                    </div>
+                    <div>
+                      <Image src={hostGroupPic2} alt="Manage necessity" />
+                      <h3>Manage necessity</h3>
+                      <p>
+                        Share a list of items you want participants to bring.
+                      </p>
+                    </div>
+                    <div>
+                      <Image src={hostGroupPic3} alt="Announcements" />
+                      <h3>Announcements</h3>
+                      <p>
+                        Send updates to participants through chat and collect
+                        their responses.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </TabsContent>
+            <TabsContent value="participants">
+              <div>
+                <Image src={guestMainPic} alt="For the participants" />
+                <h2>Easily check information, create special memories</h2>
+                <p>
+                  Keep scattered info in one place and share photos
+                  effortlessly.
+                </p>
+              </div>
+              <div>
+                <div>
+                  <h3>Check the schedule and necessity</h3>
+                  <p>
+                    Access host-shared information anytime. Engage with the host
+                    on the feed to make the event even more exciting!
+                  </p>
+                  <Image
+                    src={guestSubPic1}
+                    alt="Check announcement and necessity."
+                  />
+                </div>
+                <div>
+                  <h3>Share and view group memories in one place</h3>
+                  <p>
+                    Easily find photos of your child with AI-powered sorting.
+                  </p>
+                  <Image
+                    src={guestSubPic2}
+                    alt="Easy to find your child photos."
+                  />
+                </div>
+              </div>
+            </TabsContent>
+          </Tabs>
+        </section>
+      </section>
+      <footer className="text-text grid gap-4 px-4 py-8">
+        <div className="flex flex-col gap-6">
+          <h1 className="text-xl font-bold">Oiwai</h1>
+          <div className="text-textSub grid gap-2 text-xs">
+            <Link href="/">Privacy policy</Link>
+            <Link href="/">Terms and Conditions</Link>
+          </div>
+        </div>
+        <div className="grid gap-2">
+          <p className="text-text text-xs">Created by</p>
+          <div className="flex items-center gap-4">
+            <Link
+              href="https://www.linkedin.com/in/risa-yamamoto-profile/"
+              className="flex items-center gap-1"
+            >
+              <Image src={linkedInLogo} alt="LinkedIn" width={20} height={20} />
+              <p className="text-text text-sm">Risa</p>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/ben-ono-profile/"
+              className="flex items-center gap-1"
+            >
+              <Image src={linkedInLogo} alt="LinkedIn" width={20} height={20} />
+              <p className="text-text text-sm">Ben</p>
+            </Link>
+            <Link
+              href="http://linkedin.com/in/nao-g-55822a2bb"
+              className="flex items-center gap-1"
+            >
+              <Image src={linkedInLogo} alt="LinkedIn" width={20} height={20} />
+              <p className="text-text text-sm">Nao</p>
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/haruka-kakiuchi-profile/"
+              className="flex items-center gap-1"
+            >
+              <Image src={linkedInLogo} alt="LinkedIn" width={20} height={20} />
+              <p className="text-text text-sm">Haruka</p>
+            </Link>
+          </div>
+        </div>
       </footer>
-    </div>
+    </>
   );
 }
