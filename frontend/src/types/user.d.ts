35dbd1868eb7508dcc7b4bd7a2fac28642d59637
kakimaru <memimei0306@gmail.com>
2025-02-21 09:29:10 +0900

    feat: refine user types page

--- frontend/src/types/user.d.ts
+++ frontend/src/types/user.d.ts
@@ -1,23 +1,13 @@
-export type UserType = {
-  id: string;
-  name: string;
-  profileImageUrl: string;
-  email: string;
-  family: { id: string; profileImageUrl: string; name: string }[];
-};
-
 export type FamilyMemberType = {
   id: string;
   profileImageUrl: string;
   name: string;
 };
 
-export type MyPageUserType = {
-  user: {
-    id: string;
-    name: string;
-    profileImageUrl: string;
-    email: string;
-    userFamilies: FamilyMemberType[]
-  };
+export type UserType = {
+  id: string;
+  name: string;
+  profileImageUrl: string;
+  email: string;
+  userFamilies: FamilyMemberType[];
 };
