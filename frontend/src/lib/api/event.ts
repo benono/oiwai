331f15acb750ba872bea1ae3b56826ee9d2230ef
Risa Yamamoto <yamari2422@gmail.com>
2025-02-21 09:26:12 -0800

    chore: uncomment dummy response data

--- frontend/src/lib/api/event.ts
+++ frontend/src/lib/api/event.ts
@@ -1,23 +1,26 @@
 import { EventType } from "@/types/event";
-// import { getServerAxiosInstance } from "./axios-server";
+import { getServerAxiosInstance } from "./axios-server";
 
 // Fetch event information
 export const getEventInfo = async (
   eventId: string,
 ): Promise<{ event: EventType }> => {
   try {
-    // const axiosInstance = await getServerAxiosInstance();
-    // const response = await axiosInstance.get(`events/${eventId}`);
+    const axiosInstance = await getServerAxiosInstance();
+    const response = await axiosInstance.get(`events/${eventId}`);
+    return response.data.event;
 
-    return {
-      event: {
-        id: "12345",
-        title: "Emily’s Birthday Party🚀",
-        thumbnailUrl: "/images/sample-thumbnail.png",
-        startTime: new Date("2025-03-01T14:00:00Z"),
-        endTime: new Date("2025-03-01T16:00:00Z"),
-      },
-    };
+    // ********** Uncomment the data below to use dummy data **********
+    // return {
+    //   // Dummy data
+    //   event: {
+    //     id: "12345",
+    //     title: "Emily’s Birthday Party🚀",
+    //     thumbnailUrl: "/images/sample-thumbnail.png",
+    //     startTime: new Date("2025-03-01T14:00:00Z"),
+    //     endTime: new Date("2025-03-01T16:00:00Z"),
+    //   },
+    // };
   } catch (err) {
     if (err instanceof Error) {
       throw new Error();
--- frontend/src/lib/api/user.ts
+++ frontend/src/lib/api/user.ts
@@ -1,33 +1,35 @@
 import { UserType } from "@/types/user";
-// import { getServerAxiosInstance } from "./axios-server";
+import { getServerAxiosInstance } from "./axios-server";
 
 // Fetch user information
 export const getUserInfo = async (): Promise<{ user: UserType }> => {
   try {
-    // const axiosInstance = await getServerAxiosInstance();
-    // const response = await axiosInstance.get("/me");
+    const axiosInstance = await getServerAxiosInstance();
+    const response = await axiosInstance.get("/me");
+    return response.data.user;
 
-    return {
-      // Dummy data
-      user: {
-        id: "12345",
-        name: "Tracy",
-        profileImageUrl: "/images/sample-family-1.png",
-        email: "tracy@example.com",
-        userFamilies: [
-          {
-            id: "67890",
-            profileImageUrl: "/images/sample-family-1.png",
-            name: "Jane Doe",
-          },
-          {
-            id: "11223",
-            profileImageUrl: "/images/sample-family-2.png",
-            name: "Sam Doe",
-          },
-        ],
-      },
-    };
+    // ********** Uncomment the data below to use dummy data **********
+    // return {
+    //   // Dummy data
+    //   user: {
+    //     id: "12345",
+    //     name: "Tracy",
+    //     profileImageUrl: "/images/sample-family-1.png",
+    //     email: "tracy@example.com",
+    //     userFamilies: [
+    //       {
+    //         id: "67890",
+    //         profileImageUrl: "/images/sample-family-1.png",
+    //         name: "Jane Doe",
+    //       },
+    //       {
+    //         id: "11223",
+    //         profileImageUrl: "/images/sample-family-2.png",
+    //         name: "Sam Doe",
+    //       },
+    //     ],
+    //   },
+    // };
   } catch (err) {
     if (err instanceof Error) {
       throw new Error();
