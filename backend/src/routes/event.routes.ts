4a86ab6a49a8c375654cf2610f548818f298ed38
BenOno <bentoki.1213@gmail.com>
2025-02-25 09:23:24 -0800

    feat: add middleware

--- backend/src/routes/event.routes.ts
+++ backend/src/routes/event.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import eventController from "../controllers/event.controller";
 import timelineController from "../controllers/timeline.controller";
+import { isEventHost, isEventParticipant } from '../middleware/event.auth';
 const eventRouter = Router();
 
 // Routes
 eventRouter.get("/:event_id", eventController.getEventById);
-eventRouter.get("/:event_id/timelines", timelineController.getEventTimelines);
-eventRouter.post("/:event_id/timelines", timelineController.createTimeline);
-eventRouter.put("/:event_id/timelines/:timeline_id", timelineController.updateTimeline);
-eventRouter.delete("/:event_id/timelines/:timeline_id", timelineController.deleteTimeline);
+eventRouter.get("/:event_id/timelines", isEventParticipant, timelineController.getEventTimelines);
+eventRouter.post("/:event_id/timelines", isEventHost, timelineController.createTimeline);
+eventRouter.put("/:event_id/timelines/:timeline_id", isEventHost, timelineController.updateTimeline);
+eventRouter.delete("/:event_id/timelines/:timeline_id", isEventHost, timelineController.deleteTimeline);
 export default eventRouter;
