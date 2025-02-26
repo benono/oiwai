fe11767627d975f635ca94686efd8f4ec88f842a
Risa Yamamoto <yamari2422@gmail.com>
2025-02-26 03:11:33 -0800

    feat: add logic for deleting activity

--- frontend/src/components/features/timeline/action-dropdown.tsx
+++ frontend/src/components/features/timeline/action-dropdown.tsx
@@ -1,25 +1,62 @@
+"use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { deleteActivity } from "@/lib/actions/event/timeline";
 import { CircleX, Ellipsis, PencilLine } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export function ActionDropdown() {
+type ActionDropdownProps = {
+  eventId: string;
+  timelineId: string;
+};
+
+export function ActionDropdown({ eventId, timelineId }: ActionDropdownProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  if (!isLoading) {
+    return null;
+  }
+
+  const handleDeleteActivity = async () => {
+    const response = await deleteActivity(eventId, timelineId);
+    if (response.success) {
+      router.push(`/event/${eventId}/timeline`);
+    }
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Ellipsis />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-40">
-        <DropdownMenuItem className="flex justify-between">
-          <span className="font-medium">Edit Activity</span>
-          <PencilLine />
+        <DropdownMenuItem>
+          <Link
+            href={`/event/${eventId}/timeline/${timelineId}/edit`}
+            className="flex w-full justify-between"
+          >
+            <span className="font-medium">Edit Activity</span>
+            <PencilLine size={18} />
+          </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem className="flex justify-between">
+        <DropdownMenuItem
+          className="flex justify-between"
+          onClick={handleDeleteActivity}
+        >
           <span className="font-medium">Delete Activity</span>
-          <CircleX />
+          <CircleX size={18} />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
