dff0d301905f1a22887e5def1d54e35be488c85d
Risa Yamamoto <yamari2422@gmail.com>
2025-02-26 10:03:10 -0800

    feat: add breadcrumb for navigation

--- frontend/src/app/(auth)/event/[eventId]/timeline/create/page.tsx
+++ frontend/src/app/(auth)/event/[eventId]/timeline/create/page.tsx
@@ -1,3 +1,4 @@
+import BreadcrumbNavigation from "@/components/features/event/breadcrumb-navigation";
 import { ActivityForm } from "@/components/features/timeline/activity-form";
 
 export default async function CreateActivity({
@@ -9,6 +10,10 @@ export default async function CreateActivity({
 
   return (
     <section className="space-y-4">
+      <BreadcrumbNavigation
+        path={`/event/${eventId}/timeline`}
+        previousPageName="Timeline"
+      />
       <h1 className="text-xl font-bold">Add Activity</h1>
       <ActivityForm eventId={eventId} isCreateActivity />
     </section>
