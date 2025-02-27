e7da8a8fb5df0d9c6648411f900f4a3a832cc49b
Risa Yamamoto <yamari2422@gmail.com>
2025-02-27 14:53:33 -0800

    chore: undo unnecessary changes(modal)

--- frontend/src/components/features/timeline/action-dropdown.tsx
+++ frontend/src/components/features/timeline/action-dropdown.tsx
@@ -13,7 +13,6 @@ import { CircleX, Ellipsis, PencilLine } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import Modal from "../modal";
 
 type ActionDropdownProps = {
   eventId: string;
@@ -39,23 +38,13 @@ export function ActionDropdown({ eventId, activityId }: ActionDropdownProps) {
 
       if (response.success) {
         router.push(`/event/${eventId}/timeline`);
-        toast({
-          title: "Success",
-          description: "Activity deleted successfully",
-        });
-        return { success: true, message: "Activity deleted successfully" };
-      } else {
-        toast({ title: "Error", description: "Failed to delete activity" });
-        return {
-          success: false,
-          message: response.message || "Failed to delete activity",
-        };
       }
     } catch (err) {
       if (err instanceof Error) {
         showErrorToast(toast, err, err.message);
+      } else {
+        showErrorToast(toast, err, "Failed to delete activity");
       }
-      return { success: false, message: "Failed to delete activity" };
     }
   };
 
@@ -74,21 +63,13 @@ export function ActionDropdown({ eventId, activityId }: ActionDropdownProps) {
             <PencilLine size={18} />
           </Link>
         </DropdownMenuItem>
-        <Modal
-          trigger={
-            <DropdownMenuItem className="flex justify-between">
-              <span className="font-medium">Delete Activity</span>
-              <CircleX size={18} />
-            </DropdownMenuItem>
-          }
-          title="Delete activity"
-          description="Are you sure you want to delete this schedule? This action cannot be undone."
-          button="Delete"
-          deleteAction={handleDeleteActivity}
-          deleteErrorMessage="Something went wrong"
-          id={[activityId, eventId]}
-          onSuccess={() => console.log("success")}
-        />
+        <DropdownMenuItem
+          className="flex justify-between"
+          onClick={handleDeleteActivity}
+        >
+          <span className="font-medium">Delete Activity</span>
+          <CircleX size={18} />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
