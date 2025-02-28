2a34b686515525030271607dce6972fd3e9f502b
BenOno <bentoki.1213@gmail.com>
2025-02-28 06:02:16 -0800

    feat: add isHost api

--- backend/src/controllers/event.controller.ts
+++ backend/src/controllers/event.controller.ts
@@ -1,22 +1,36 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import eventModel from "../models/event.model";
+import { checkIsRequestFromHost } from "../utils/request-checker";
 
 // Get event infomation
-const getEventById = async (req: Request<{ event_id: string }>, res: Response) => {
-    try {
-      const id = Number(req.params.event_id)
-      const event = await eventModel.fetchEventById(id)
-      if (!event) {
-        res.status(404).json({ error: 'Event not found' })
-        return
-      }
-      res.status(200).json({event})
-    } catch (err) {
-      console.error(err)
-      res.status(500).json({ error: 'Unable to get event by id' })
+const getEventById = async (
+  req: Request<{ event_id: string }>,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const id = Number(req.params.event_id);
+    const event = await eventModel.fetchEventById(id);
+    if (!event) {
+      res.status(404).json({ error: "Event not found" });
+      return;
     }
+    res.status(200).json({ event });
+  } catch (err) {
+    next(err);
   }
+};
+
+const checkIsEventHost = async (req: Request<{ event_id: string }>, res: Response, next: NextFunction) => {
+  try {
+    const isHost = await checkIsRequestFromHost(req);
+    res.status(200).json({ isHost });
+  } catch (err) {
+    next(err);
+  }
+};
 
 export default {
-    getEventById,
-}
+  getEventById,
+  checkIsEventHost,
+};
--- backend/src/routes/event.routes.ts
+++ backend/src/routes/event.routes.ts
@@ -7,6 +7,7 @@ const eventRouter = Router();
 
 // Routes
 eventRouter.get("/:event_id", isEventHostOrParticipant, eventController.getEventById);
+eventRouter.get("/:event_id/is-host", isEventHostOrParticipant, eventController.checkIsEventHost);
 
 // Timelines
 eventRouter.get(
