85060b2652cc7b3241a2a6b7da66924f7fa7b500
Risa Yamamoto <yamari2422@gmail.com>
2025-02-28 11:11:15 -0800

    fix: change the props for breadcrumb page depending on the existence of timeline data

--- frontend/src/app/(auth)/event/[eventId]/timeline/create/page.tsx
+++ frontend/src/app/(auth)/event/[eventId]/timeline/create/page.tsx
@@ -1,5 +1,6 @@
 import BreadcrumbNavigation from "@/components/features/event/breadcrumb-navigation";
 import { ActivityForm } from "@/components/features/timeline/activity-form";
+import { getTimeline } from "@/lib/api/timeline";
 
 export default async function CreateActivity({
   params,
@@ -7,13 +8,16 @@ export default async function CreateActivity({
   params: Promise<{ eventId: string }>;
 }) {
   const { eventId } = await params;
+  const { timelines } = await getTimeline(eventId);
+
+  const breadcrumbProps =
+    timelines.length === 0
+      ? { path: `/event/${eventId}`, previousPageName: "Event home" }
+      : { path: `/event/${eventId}/timeline`, previousPageName: "Timeline" };
 
   return (
     <section className="space-y-4">
-      <BreadcrumbNavigation
-        path={`/event/${eventId}/timeline`}
-        previousPageName="Timeline"
-      />
+      <BreadcrumbNavigation {...breadcrumbProps} />
       <h1 className="text-xl font-bold">Add Activity</h1>
       <ActivityForm eventId={eventId} isCreateActivity />
     </section>
