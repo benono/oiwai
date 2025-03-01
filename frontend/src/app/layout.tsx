6654dccffe8cec9d5432c79618fefd4d128e1567
Risa Yamamoto <yamari2422@gmail.com>
2025-02-28 17:14:16 -0800

    chore: revert clerk provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Header from "@/components/layouts/header";
 import { Toaster } from "@/components/ui/toaster";
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
 
 const quicksand = Quicksand({
   variable: "--font-quick-sans",
@@ -23,14 +23,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${quicksand.className} antialiased`}>
-        <Providers>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body className={`${quicksand.className} antialiased`}>
           <Header />
           <main>{children}</main>
           <Toaster />
-        </Providers>
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
