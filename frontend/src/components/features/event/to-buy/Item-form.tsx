9a34ec5ea4a140da7863daad52ec5daf91eedde9
Risa Yamamoto <yamari2422@gmail.com>
2025-03-02 14:27:18 -0800

    feat: add function for tracking and update remain budget

--- frontend/src/components/features/event/to-buy/Item-form.tsx
+++ frontend/src/components/features/event/to-buy/Item-form.tsx
@@ -19,7 +19,8 @@ import { showErrorToast } from "@/lib/toast/toast-utils";
 import { BudgetType, ShoppingItemType } from "@/types/to-buy";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/navigation";
-import { FormProvider, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+import { FormProvider, useForm, useWatch } from "react-hook-form";
 import { z } from "zod";
 
 type ItemFormProps = {
@@ -48,7 +49,6 @@ export default function ItemForm({
   isInitialCreate,
 }: ItemFormProps) {
   const router = useRouter();
-
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
@@ -57,6 +57,17 @@ export default function ItemForm({
       quantity: thingToBuy ? thingToBuy.quantity : 0,
     },
   });
+
+  const [calculatedRemainBudget, setCalculatedRemainBudget] =
+    useState(remainBudget);
+  const price = useWatch({ control: form.control, name: "price" });
+  const quantity = useWatch({ control: form.control, name: "quantity" });
+
+  // Track and update the remaining budget
+  useEffect(() => {
+    const totalCost = price * quantity;
+    setCalculatedRemainBudget(remainBudget - totalCost);
+  }, [price, quantity, remainBudget]);
 
   const onSubmit = async (data: z.infer<typeof FormSchema>) => {
     try {
@@ -124,16 +135,31 @@ export default function ItemForm({
             <FormItem>
               <FormLabel className="font-semibold">Price</FormLabel>
               <FormControl>
-                <Input
-                  {...field}
-                  type="number"
-                  placeholder="Enter price"
-                  className="h-12 font-medium placeholder:text-textSub"
-                />
+                <>
+                  <Input
+                    {...field}
+                    type="number"
+                    placeholder="Enter price"
+                    className="h-12 font-medium placeholder:text-textSub"
+                  />
+                  <p
+                    className={`text-sm font-semibold ${calculatedRemainBudget < 0 ? "text-red-500" : ""}`}
+                  >
+                    {calculatedRemainBudget < 0 ? (
+                      `You have exceeded your budget by $${Math.abs(calculatedRemainBudget)}`
+                    ) : (
+                      <>
+                        You have
+                        <span className="text-accentGreen">
+                          {" "}
+                          ${calculatedRemainBudget}{" "}
+                        </span>
+                        left in your budget.
+                      </>
+                    )}
+                  </p>
+                </>
               </FormControl>
-              <p>
-                You have $ <span>{remainBudget}</span> left in your budget.
-              </p>
               <FormMessage />
             </FormItem>
           )}
