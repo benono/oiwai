9dec3cf4012151b7ff2663f3b3f29c9a8d2ba652
Risa Yamamoto <yamari2422@gmail.com>
2025-03-06 22:56:22 -0800

    feat: add input fields

--- frontend/src/app/(auth)/event/create/page.tsx
+++ frontend/src/app/(auth)/event/create/page.tsx
@@ -1,87 +1,268 @@
 "use client";
 
-import MapWithMarkers from "@/components/event/map-with-markers";
-import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
+import { cn } from "@/lib/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
+import { CalendarIcon, PlusIcon } from "lucide-react";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-interface Place {
-  id: string;
-  name: string;
-  location: { lat: number; lng: number };
-  address: string;
-  type: string;
-}
+const formSchema = z.object({
+  title: z.string().min(1, "Title is required"),
+  image: z.custom<File[]>().optional(),
+  date: z.date({
+    required_error: "A date of an event is required.",
+  }),
+  eventType: z.string({
+    required_error: "Please select an eventType to display.",
+  }),
+  options: z.boolean({
+    required_error: "Please select an options to display.",
+  }),
+  theme: z.string({
+    required_error: "Please select a theme to display.",
+  }),
+});
+
+type FormData = z.infer<typeof formSchema>;
 
 export default function CreateEventPage() {
-  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
-  const [places, setPlaces] = useState<Place[]>([]);
-  const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
-  const [location, setLocation] = useState({ lat: 35.6812, lng: 139.7671 }); // 東京
+  const inputImageRef = useRef<HTMLInputElement>(null);
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
-  // ユーザーの現在位置を取得
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          setLocation({
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          });
-        },
-        (error) => {
-          console.error("Error getting current location:", error);
-        },
-      );
+  const form = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      form.setValue("image", [file]);
+      const imageUrl = URL.createObjectURL(file);
+      setImagePreview(imageUrl);
     }
-  }, []);
+  };
 
-  const searchCurrentLocation = async () => {
-    const response = await fetch(
-      `/api/geocode/route?query=${location.lat},${location.lng}`,
-    );
-    const data = await response.json();
-    setPlaces(data.places);
+  const handleImageClick = () => {
+    inputImageRef.current?.click();
+  };
+
+  const onSubmit = (data: FormData) => {
+    console.log("Form submitted:", data);
+    alert("Event Created Successfully!");
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="mb-6 text-2xl font-bold">Create New Event</h1>
+    <section className="container mx-auto space-y-6 p-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="image"
+            render={({ field }) => (
+              <FormItem>
+                <div
+                  className="relative flex h-[240px] w-full cursor-pointer flex-col items-center justify-center rounded-lg"
+                  onClick={handleImageClick}
+                >
+                  {imagePreview ? (
+                    <Image
+                      src={imagePreview}
+                      alt="Preview"
+                      width={500}
+                      height={500}
+                      className="rounded object-cover"
+                    />
+                  ) : (
+                    <div className="relative h-full w-full rounded-lg border-2 border-dashed border-gray-400 bg-background">
+                      <div className="absolute inset-0 flex items-center justify-center">
+                        <PlusIcon className="h-8 w-8 text-textSub" />
+                      </div>
+                      <p className="absolute bottom-2 w-full text-center text-sm font-bold text-textSub">
+                        Add an image of the guest of honor!
+                      </p>
+                    </div>
+                  )}
+                  <FormControl>
+                    <input
+                      type="file"
+                      ref={(e) => {
+                        field.ref(e);
+                        inputImageRef.current = e;
+                      }}
+                      className="hidden"
+                      accept="image/*"
+                      onChange={(e) => {
+                        field.onChange(e.target.files);
+                        handleImageChange(e);
+                      }}
+                    />
+                  </FormControl>
+                </div>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <input
+                    type="text"
+                    placeholder="Add Event Title"
+                    className="w-full border-gray-300 py-4 text-2xl font-bold outline-none placeholder:text-textBorder focus:border-black"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date and Time</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl className="w-full bg-white">
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "h-10 w-full pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground",
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      <div className="mb-4">
-        <button
-          onClick={searchCurrentLocation}
-          className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
-        >
-          Find Nearby Parks
-        </button>
-      </div>
+          <FormField
+            control={form.control}
+            name="eventType"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Event type</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl className="h-10">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select an event type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="m@example.com">Home party</SelectItem>
+                    <SelectItem value="m@google.com">Outside</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-      <MapWithMarkers
-        apiKey={googleMapsApiKey}
-        center={location}
-        places={places}
-        onPlaceSelect={(place) => setSelectedPlace(place)}
-      />
+          <FormField
+            control={form.control}
+            name="options"
+            render={({ field }) => (
+              <FormItem className="flex flex-row items-center justify-between rounded-lg border p-2 shadow-sm">
+                <FormLabel>Ask allergies or dietary restrictions</FormLabel>
+                <FormControl>
+                  <Switch
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                    className="m-0 p-0"
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-      {selectedPlace && (
-        <div className="mt-4 rounded border bg-green-50 p-4">
-          <h2 className="mb-2 text-lg font-semibold">Selected Venue</h2>
-          <p>
-            <strong>Name:</strong> {selectedPlace.name}
-          </p>
-          <p>
-            <strong>Address:</strong> {selectedPlace.address}
-          </p>
-          <button
-            className="mt-2 rounded bg-green-500 px-4 py-2 text-white hover:bg-green-600"
-            onClick={() => {
-              // イベント作成フォームに選択した場所の情報を設定する処理
-              console.log("Selected place for event:", selectedPlace);
-            }}
+          <FormField
+            control={form.control}
+            name="theme"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Invitation Theme</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl className="h-10">
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select an invitation theme" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectItem value="Orange">Orange</SelectItem>
+                    <SelectItem value="Blue">Blue</SelectItem>
+                    <SelectItem value="Simple">Simple</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button
+            type="submit"
+            className="h-12 w-full rounded-full text-base font-bold"
           >
-            Use This Location
-          </button>
-        </div>
-      )}
-    </div>
+            Create invitation
+          </Button>
+        </form>
+      </Form>
+    </section>
   );
 }
