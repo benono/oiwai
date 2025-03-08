a27de4cf033380b586bf66d0406c400c69eca869
kakimaru <memimei0306@gmail.com>
2025-03-08 14:04:14 +0900

    feat: add templates

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -4,7 +4,44 @@ import { EmailService } from "../utils/email";
 const getuserById = async (req: Request, res: Response) => {
   try {
     const emailService = new EmailService();
-    await emailService.sendWelcome("Event Name", "Inviter Name");
+    await emailService.sendGuestAttending({
+      eventName: "Event Name",
+      eventDate: "2025-12-20",
+      inviterName: "Inviter Name",
+      eventId: "eventId",
+      eventImage: "eventImage",
+      eventLocation: "Tokyo",
+    });
+    await emailService.sendOneDayBeforeReminder({
+      eventName: "Event Name",
+      eventDate: "2025-12-20",
+      eventLocation: "Tokyo",
+      eventId: "eventId",
+      eventImage: "eventImage",
+    });
+    await emailService.sendDayOfReminder({
+      eventName: "Event Name",
+      eventDate: "2025-12-20",
+      eventLocation: "Tokyo",
+      eventId: "eventId",
+      eventImage: "eventImage",
+      eventStartTime: "4:00 PM",
+    });
+    await emailService.sendAfterEvent({
+      eventName: "Event Name",
+      eventId: "eventId",
+    });
+    await emailService.sendAnnouncement({
+      eventName: "Event Name",
+      inviterName: "Inviter Name",
+      eventDate: "Event Date",
+      eventLocation: "Event Location",
+      eventId: "Event Id",
+      eventImage: "Event Image",
+      inviterImage: "Inviter Image",
+      announcement:
+        "Hi, everyone! The event is coming tomorrow. Can't wait to see you all :) ",
+    });
     res.status(200).json({ message: "Email sent successfully" });
   } catch (err) {
     console.error(err);
--- backend/src/utils/email.ts
+++ backend/src/utils/email.ts
@@ -1,6 +1,10 @@
 import { renderAsync } from "@react-email/render";
 import { Resend } from "resend";
-import { WelcomeEmail } from "./emails/templates/welcome";
+import { AfterEvent } from "./emails/templates/after-event";
+import { Announcement } from "./emails/templates/announcement";
+import { DayOfReminder } from "./emails/templates/day-of-reminder";
+import { GuestAttending } from "./emails/templates/guest-attending";
+import { OneDayBeforeReminder } from "./emails/templates/one-day-before-reminder";
 
 export class EmailService {
   private resend: Resend;
@@ -8,31 +12,186 @@ export class EmailService {
 
   constructor() {
     this.resend = new Resend(process.env.RESEND_API_KEY);
-    this.from = "oiwaii@resend.dev";
+    this.from = "oiwai@resend.dev";
   }
-  async sendWelcome(eventName: string, inviterName: string) {
+
+  async sendGuestAttending({
+    eventName,
+    eventDate,
+    eventLocation,
+    eventId,
+    eventImage,
+  }: {
+    eventName: string;
+    eventDate: string;
+    eventLocation: string;
+    inviterName: string;
+    eventId: string;
+    eventImage: string;
+  }) {
     try {
       const html = await renderAsync(
-        WelcomeEmail({
+        GuestAttending({
           eventName,
-          inviterName,
-          eventDate: new Date().toISOString(),
-          eventLocation: "Tokyo",
+          eventDate,
+          eventLocation,
+          eventId,
+          eventImage,
         }),
       );
       return await this.resend.emails.send({
         from: this.from,
-        to: "bentoki.1213@gmail.com",
-        subject: `${inviterName} accept your invitation`,
+        to: "memimei0306@gmail.com",
+        subject: `You're in! ${eventName} details inside 🚀`,
         html,
       });
     } catch (error) {
-      console.error("Failed to send invitation email:", error);
+      console.error("Failed to send guest attending mail:", error);
       throw error;
     }
   }
 
-  async sendEventReminder(to: string, eventName: string, eventDate: Date) {
-    // implement event reminder
+  async sendOneDayBeforeReminder({
+    eventName,
+    eventDate,
+    eventId,
+    eventImage,
+    eventLocation,
+  }: {
+    eventName: string;
+    eventDate: string;
+    eventId: string;
+    eventImage: string;
+    eventLocation: string;
+  }) {
+    try {
+      const html = await renderAsync(
+        OneDayBeforeReminder({
+          eventName,
+          eventDate,
+          eventLocation,
+          eventId,
+          eventImage,
+          reminderDate: "1 day",
+        }),
+      );
+      return await this.resend.emails.send({
+        from: this.from,
+        to: "memimei0306@gmail.com",
+        subject: `Reminder for ${eventName}! (1 day before)`,
+        html,
+      });
+    } catch (error) {
+      console.error("Failed to send one day before reminder mail:", error);
+      throw error;
+    }
+  }
+
+  async sendDayOfReminder({
+    eventName,
+    eventDate,
+    eventId,
+    eventImage,
+    eventLocation,
+    eventStartTime,
+  }: {
+    eventName: string;
+    eventDate: string;
+    eventId: string;
+    eventImage: string;
+    eventLocation: string;
+    eventStartTime: string;
+  }) {
+    try {
+      const html = await renderAsync(
+        DayOfReminder({
+          eventName,
+          eventDate,
+          eventLocation,
+          eventId,
+          eventImage,
+          eventStartTime,
+        }),
+      );
+      return await this.resend.emails.send({
+        from: this.from,
+        to: "memimei0306@gmail.com",
+        subject: `Reminder for ${eventName}! (Today)`,
+        html,
+      });
+    } catch (error) {
+      console.error("Failed to send day of reminder mail:", error);
+      throw error;
+    }
+  }
+
+  async sendAfterEvent({
+    eventName,
+    eventId,
+  }: {
+    eventName: string;
+    eventId: string;
+  }) {
+    try {
+      const html = await renderAsync(
+        AfterEvent({
+          eventName,
+          eventId,
+        }),
+      );
+      return await this.resend.emails.send({
+        from: this.from,
+        to: "memimei0306@gmail.com",
+        subject: `✨ Thanks for joining ${eventName}! ✨`,
+        html,
+      });
+    } catch (error) {
+      console.error("Failed to after event mail:", error);
+      throw error;
+    }
+  }
+
+  async sendAnnouncement({
+    eventName,
+    inviterName,
+    eventDate,
+    eventLocation,
+    eventId,
+    eventImage,
+    inviterImage,
+    announcement,
+  }: {
+    eventName: string;
+    inviterName: string;
+    eventDate: string;
+    eventLocation: string;
+    eventId: string;
+    eventImage: string;
+    inviterImage: string;
+    announcement: string;
+  }) {
+    try {
+      const html = await renderAsync(
+        Announcement({
+          eventName,
+          inviterName,
+          eventDate,
+          eventLocation,
+          eventId,
+          eventImage,
+          inviterImage,
+          announcement,
+        }),
+      );
+      return await this.resend.emails.send({
+        from: this.from,
+        to: "memimei0306@gmail.com",
+        subject: `【${eventName}】New Message from the host 📣`,
+        html,
+      });
+    } catch (error) {
+      console.error("Failed to announcement mail:", error);
+      throw error;
+    }
   }
 }
