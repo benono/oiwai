aad6d54810262be76e5de0c678c419a533182c82
Risa Yamamoto <yamari2422@gmail.com>
2025-03-12 10:31:30 -0700

    chore: apply the auto format setting

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,13 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher(['/event(.*)', '/my-page(.*)'])
+const isProtectedRoute = createRouteMatcher(["/event(.*)", "/my-page(.*)"]);
 
 const isPublicRoute = createRouteMatcher(["/", "/rsvp(.*)", "/sign-in(.*)"]);
 
-
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth()
+  const { userId } = await auth();
   const currentPath = req.nextUrl.pathname;
 
   if (userId && isPublicRoute(req) && currentPath !== "/my-page") {
@@ -18,14 +17,14 @@ export default clerkMiddleware(async (auth, req) => {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  return NextResponse.next()
-})
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
