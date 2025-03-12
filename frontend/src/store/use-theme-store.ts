bbf02fabf7a6f0cf66aaabefbdefe444a64d67be
Risa Yamamoto <yamari2422@gmail.com>
2025-03-12 15:07:44 -0700

    refactor: set default value of type

--- frontend/src/store/use-theme-store.ts
+++ frontend/src/store/use-theme-store.ts
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 interface ThemeState {
   themeColor: string;
-  setThemeColor: (theme: string | null) => void;
+  setThemeColor: (theme: string) => void;
 }
 
 export const useThemeStore = create<ThemeState>((set) => ({
