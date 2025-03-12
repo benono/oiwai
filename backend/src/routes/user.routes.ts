932909e70ca18c4447fb78b09a8da9c12f37bdae
BenOno <bentoki.1213@gmail.com>
2025-03-11 23:21:38 -0700

    chore: merge dev's route

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,43 @@
 import { Router } from "express";
 import usersController from "../controllers/user.controller";
 import requireAuthMiddleware from "../middleware/auth";
+import upload from "../middleware/uploadMiddleware";
 
 const usersRouter = Router();
 
 // Routes
 usersRouter.get("/", requireAuthMiddleware, usersController.getUserById);
+usersRouter.get(
+  "/events",
+  requireAuthMiddleware,
+  usersController.getEventInfoByEmail,
+);
+usersRouter.patch(
+  "/",
+  requireAuthMiddleware,
+  upload.single("profileImage"),
+  usersController.updateUser,
+);
+usersRouter.delete("/", requireAuthMiddleware, usersController.deleteUser);
+
+usersRouter.post(
+  "/family/",
+  requireAuthMiddleware,
+  upload.single("profileImage"),
+  usersController.addNewUserFamily,
+);
+
+usersRouter.patch(
+  "/family/:family_id",
+  requireAuthMiddleware,
+  upload.single("profileImage"),
+  usersController.updateUserFamily,
+);
+
+usersRouter.delete(
+  "/family/:family_id",
+  requireAuthMiddleware,
+  usersController.deleteUserFamily,
+);
 
 export default usersRouter;
