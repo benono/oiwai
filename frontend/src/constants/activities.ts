f37fffb559404c84e245047dce8e054dd36eb8ab
Risa Yamamoto <yamari2422@gmail.com>
2025-03-13 19:56:04 -0700

    feat: add activity config

---
+++ frontend/src/constants/activities.ts
@@ -0,0 +1,30 @@
+export const ACTIVITY_LIST = [
+  {
+    name: "Picnic",
+    image: "/images/picnic.png",
+  },
+  {
+    name: "Hiking",
+    image: "/images/hiking.png",
+  },
+  {
+    name: "Mini golf",
+    image: "/images/mini-golf.png",
+  },
+  {
+    name: "Baseball",
+    image: "/images/baseball.png",
+  },
+  {
+    name: "Camping",
+    image: "/images/camping.png",
+  },
+  {
+    name: "Swimming",
+    image: "/images/swimming.png",
+  },
+  {
+    name: "Cherry blossom viewing",
+    image: "/images/cherryBlossom.png",
+  },
+];
