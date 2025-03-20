51a8dd311be8db1df8799b33d25cff6423c553b9
Risa Yamamoto <yamari2422@gmail.com>
2025-03-20 09:50:02 -0700

    chore: add pink to the theme option

--- frontend/src/constants/theme-colors.ts
+++ frontend/src/constants/theme-colors.ts
@@ -2,6 +2,7 @@ export const THEME_BACKGROUND_COLORS: Record<string, string> = {
   "#F97315": "#FFFBF2",
   "#1A74A2": "#F2F7FF",
   "#7E4F8F": "#F6F2FF",
+  "#E0487B": "#FFF7FA",
 };
 
 export const THEME_CONFIG = {
@@ -16,5 +17,9 @@ export const THEME_CONFIG = {
   purple: {
     color: "#7E4F8F",
     className: "bg-accentPurple",
+  },
+  pink: {
+    color: "#E0487B",
+    className: "bg-accentPink",
   },
 };
