ddeeca37473a3c3147e8a177b32fb6a8e09c3ac5
Risa Yamamoto <yamari2422@gmail.com>
2025-03-21 16:15:42 -0700

    style: add an animation when the icons are hovered

--- frontend/src/components/features/event/menu-icon.tsx
+++ frontend/src/components/features/event/menu-icon.tsx
@@ -18,10 +18,11 @@ export default function MenuIcon({ iconDetail, eventId }: MenuIconProps) {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <Link href={`/event/${eventId}/${iconDetail.path}`} >
-        <div
-          className={`flex h-14 w-14 items-center justify-center rounded-full bg-${iconDetail.backgroundColor}`}
-        >
+      <Link href={`/event/${eventId}/${iconDetail.path}`} className="group">
+        <div className="relative flex h-14 w-14 items-center justify-center transition-transform duration-200 group-hover:scale-110">
+          <div
+            className={`absolute inset-0 rounded-full bg-${iconDetail.backgroundColor}`}
+          />
           <IconComponent size={24} className={`text-${iconDetail.iconColor}`} />
         </div>
         <p className="text-center text-sm font-semibold">
